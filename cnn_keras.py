5dd5571ae3578dce3bebe5990c766af023f07793
Nicolas <nfonseca1999@gmail.com>
2022-10-13 20:49:42 -0500

    Se inicializa un proyecto de CNN con clasificador

---
+++ cnn_keras.py
@@ -0,0 +1,10 @@
+import os
+import tensorflow as tf
+print(tf. __version__)
+import cv2
+print (cv2. __version__)
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
