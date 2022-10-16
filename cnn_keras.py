da98e73aabd7abe0f5277ba4323507f11c37d816
Nicolas <nfonseca1999@gmail.com>
2022-10-16 16:48:28 -0500

    Se inserta el dataset seleccionado, se agregan direcciones de train y validación

--- cnn_keras.py
+++ cnn_keras.py
@@ -1,10 +1,27 @@
 import os
 import tensorflow as tf
-print(tf. __version__)
-import cv2
-print (cv2. __version__)
+import cv2 #pip install opencv-python - si es necesario cambiar el interprete (ctrl + shift + p))      
 import matplotlib.pyplot as plt
 import numpy as np
-from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+#from tensorflow.keras.callbacks import TensorBoard
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#Para los dataset consultar en Tensorflow datasets (https://www.tensorflow.org/datasets/catalog/overview)
+
+#Dirección de imagenes para entrenamiento y validación
+entrenamiento = 'C:\Users\nfons\Documents\Universidad\Proyecto de grado\Redes\Dataset\Entrenamiento'
+validacion = 'C:\Users\nfons\Documents\Universidad\Proyecto de grado\Redes\Dataset\Validacion'
+
+ListaTrain = os.listdir(entrenamiento)
+ListaTest = os.listdir(validacion)
+
+#Se establecen parametros
+ancho, alto = 200, 200
+
+#listas para entrenamiento
+etiquetas = []
+fotos = []
+datos_train = []
+con = 
+
 
